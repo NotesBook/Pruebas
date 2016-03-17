2c3386c7a2ef7e79d1a863ef39caa47b103f24fb
galo.aragoneses <galo.aragoneses@gmail.com>
2016-03-17 11:13:13 +0100

    Prueba bbdd migrations - Mysql

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php 
+/* BBDD - Config the connection */
+$bbdd_server = "localhost"; 
+$bbdd_user = "root"; 
+$bbdd_password = ""; 
+$bbdd_name = "notesbook"; 
+ ?>
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php 
+	/* Este fichero contiene todas las variales de configuración global de la applicación */
+	include "./config.php";
+
+	/* Este fichero, se encarga de mantener la bbdd */
+	include "./bbdd_migrations.php"; //En un futuro esta llamada estará incluido en un fichero que se ejecutará en cualquier página
+ ?>
